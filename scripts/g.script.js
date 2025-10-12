284e2923228e9f576f1a894eb7c757447b65e00c
XxBrandon-75 <brandonrgz06@gmail.com>
2025-10-11 23:13:40 -0600

    Gran Calvario

---
+++ scripts/g.script.js
@@ -0,0 +1,9 @@
+let menubar = document.querySelector('#menu-bar')
+let mynav = document.querySelector('.barra-navegacion')
+
+
+
+menubar.onclick = ()=>{
+    menubar.classList.toggle('fa-times')
+    mynav.classList.toggle('active')
+}
